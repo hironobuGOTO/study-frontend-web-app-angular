a74e76e52c0badeecbff7282f6674c795bafab31
hironobuGOTO <r23obin6@gmail.com>
2022-04-09 15:16:43 +0900

    Angularの事始め

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { animationFrameScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  tasks = [
+    {
+      name: 'ライブの申し込み',
+      isCompleted: true,
+    },
+    {
+      name: 'セトリの予想',
+      isCompleted: false,
+    },
+  ];
+
+  addTask(taskName: string) {
+    this.tasks.push({
+      name: taskName,
+      isCompleted: false,
+    });
+  }
+
+  getNumOfCompletedTasks() {
+    let count = 0;
+    for (let task of this.tasks) {
+      if (task.isCompleted) {
+        count++;
+      }
+    }
+    return count;
+  }
 }
