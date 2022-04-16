e2a12dc02e03c99793511f824cb687974b83792b
hironobuGOTO <r23obin6@gmail.com>
2022-04-16 14:55:51 +0900

    爆速でタスクを入力できるようにした

--- src/app/task-edit/task-edit.component.ts
+++ src/app/task-edit/task-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { TaskManagerService } from '../task-manager.service';
 import { Router } from '@angular/router';
 
@@ -7,8 +14,21 @@ import { Router } from '@angular/router';
   templateUrl: './task-edit.component.html',
   styleUrls: ['./task-edit.component.scss'],
 })
-export class TaskEditComponent implements OnInit {
-  constructor(public taskManager: TaskManagerService, private router: Router) {}
+export class TaskEditComponent implements OnInit, AfterViewInit {
+  @ViewChild('addTaskNameInput') taskNameInputElem!: ElementRef;
+
+  constructor(
+    public taskManager: TaskManagerService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private router: Router
+  ) {}
+
+  ngAfterViewInit(): void {
+    // タスク名入力欄にフォーカスをあてる
+    this.taskNameInputElem.nativeElement.focus();
+    // Angular に強制的に変更検出させる
+    this.changeDetectorRef.detectChanges();
+  }
 
   ngOnInit(): void {}
 
